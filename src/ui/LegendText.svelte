55faa13f56de5c8ad660dba795a10f7736af6022
rpgraffi <rpgmelone@gmail.com>
2024-01-17 14:06:24 +0100

    formatting

--- src/ui/LegendText.svelte
+++ src/ui/LegendText.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { onMount } from 'svelte';
 	import { themes } from '../config.js';
 
 	export let text1;
@@ -7,37 +6,37 @@
 	export let text3;
 	export let text4;
 	export let color1 = themes.oepnv.primary;
-	export let color2  = themes.bike.primary;
-	export let color3  = themes.car.primary;
-	export let color4  = themes.neutral['text-dark'].secondary;
+	export let color2 = themes.bike.primary;
+	export let color3 = themes.car.primary;
+	export let color4 = themes.neutral['text-dark'].secondary;
 </script>
 
 {#if text1 || text2 || text3 || text4}
 	<div class="legend">
 		{#if text1}
-        <div class="horizontal">
-            <div class="bar" style="background-color: {color1};"></div>
-			<p style="color: {themes.neutral['text-dark'].secondary};">{text1}</p>
-        </div>
+			<div class="horizontal">
+				<div class="bar" style="background-color: {color1};"></div>
+				<p style="color: {themes.neutral['text-dark'].secondary};">{text1}</p>
+			</div>
 		{/if}
 		{#if text2}
-        <div class="horizontal">
-            <div class="bar" style="background-color: {color2};"></div>
-			<p style="color: {themes.neutral['text-dark'].secondary};">{text2}</p>
-        </div>
+			<div class="horizontal">
+				<div class="bar" style="background-color: {color2};"></div>
+				<p style="color: {themes.neutral['text-dark'].secondary};">{text2}</p>
+			</div>
 		{/if}
 		{#if text3}
-        <div class="horizontal">    
-            <div class="bar" style="background-color: {color3};"></div>
-			<p style="color: {themes.neutral['text-dark'].secondary};">{text3}</p>
-        </div>
+			<div class="horizontal">
+				<div class="bar" style="background-color: {color3};"></div>
+				<p style="color: {themes.neutral['text-dark'].secondary};">{text3}</p>
+			</div>
 		{/if}
 		{#if text4}
-        <div class="horizontal">
-            <div class="bar" style="background-color: {color4};"></div>
-			<p style="color: {themes.neutral['text-dark'].secondary};">{text4}</p>
-        </div>
-            {/if}
+			<div class="horizontal">
+				<div class="bar" style="background-color: {color4};"></div>
+				<p style="color: {themes.neutral['text-dark'].secondary};">{text4}</p>
+			</div>
+		{/if}
 	</div>
 {/if}
 
@@ -61,10 +60,10 @@
 		height: 12px;
 		border-radius: 2px;
 	}
-    .horizontal {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 4px;
-    }   
+	.horizontal {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 4px;
+	}
 </style>
